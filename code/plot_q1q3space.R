5aab8775c593d0a3a011ebe6d05ab0088614eda8
Hannes Roest <hroest@rinaldo.(none)>
2010-07-19 18:16:56 +0200

    png to pdf

--- code/plot_q1q3space.R
+++ code/plot_q1q3space.R
@@ -3,20 +3,28 @@ dbh <- dbConnect( MySQL(),default.file='/etc/httpd/conf/.my.cnf')
 library(plotrix, lib.loc = '/projects/compep/R/')
 library(hexbin)
 
-df <- dbGetQuery(dbh, "select * from hroest.srmClashes
+df <- dbGetQuery(dbh, "
+select * from hroest.srmClashesHuman
 where experiment_key = 3061 
-and q1 between 300 and 1500 
-and q3 between 300 and 1500 
-#limit 100000
-" )
+and q1 between 400 and 1500 
+and q3 between 400 and 1500 
+")
 
 #plot the q1-q3 space using hexbins
-pdf(file="./results/q1_q3_hexbins_120_human.pdf" )
-hxbinobj = hexbin(x=df[,7], y=df[,8], xbins=120 )
+pdf(file="./results/q1_q3_hexbins_120_human_small.pdf" )
+hxbinobj = hexbin(x=df[,7], y=df[,8], xbins=110 )
 plot( hxbinobj, xlab="", ylab="" )
 dev.off()
-pdf(file="./results/q1_q3_hexbins_1200_human.pdf" )
-hxbinobj = hexbin(x=df[,7], y=df[,8], xbins=1200 )
+
+pdf(file="./results/q1_q3_hexbins_1200_human_small.pdf" )
+
+hxbinobj = hexbin(x=df[,7], y=df[,8], xbins=1100 )
+
+png(file="./results/q1_q3_hexbins_1200_human_small_bigfile.png", 
+    width = 5000, height = 5000, units = "px", res = 500 )
+plot( hxbinobj , xlab="", ylab="")
+dev.off()
+pdf(file="./results/q1_q3_hexbins_1200_human_small.pdf" )
 plot( hxbinobj , xlab="", ylab="")
 dev.off()
 
