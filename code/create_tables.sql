1afa091d46315c84e4a52baeaed62c2f521704e4
Hannes Roest <hroest@rinaldo.(none)>
2010-08-20 17:04:11 +0200

    maintenance

--- code/create_tables.sql
+++ code/create_tables.sql
@@ -1,5 +1,5 @@
-#-- srmPeptide contains peptide information 
-#-- srmTransitions contains transitions
+#-- srmPeptides_test contains peptide information 
+#-- srmTransitions_test contains transitions
 #-- they are joined over parent_id = parent_key
 
 #-- peptide_key links the peptides to the ddb.peptide table
@@ -7,40 +7,40 @@
 
 #-- old table == srmClashes (non-normalized; contains ALL transitions)
 
-#drop table hroest.srmPeptide;
-#truncate table hroest.srmPeptide;
-create table hroest.srmPeptide(
+#drop table hroest.srmPeptides_test;
+truncate table hroest.srmPeptides_test;
+create table hroest.srmPeptides_test(
     parent_id INT PRIMARY KEY AUTO_INCREMENT,
     peptide_key INT,
     q1_charge INT,
     q1 DOUBLE,
     ssrcalc DOUBLE
 );
-alter table hroest.srmPeptide add index(peptide_key);
-alter table hroest.srmPeptide add index(q1);
-alter table hroest.srmPeptide add index(ssrcalc);
+alter table hroest.srmPeptides_test add index(peptide_key);
+alter table hroest.srmPeptides_test add index(q0);
+alter table hroest.srmPeptides_test add index(ssrcalc);
 
-#drop table hroest.srmTransitions;
-#truncate table hroest.srmTransitions;
-create table hroest.srmTransitions(
+#drop table hroest.srmTransitions_test;
+truncate table hroest.srmTransitions_test;
+create table hroest.srmTransitions_test(
     srm_id INT PRIMARY KEY AUTO_INCREMENT,
     parent_key INT,
     q3_charge INT,
     q3 DOUBLE,
     type VARCHAR(3)
 );
-alter table hroest.srmTransitions add index(parent_key);
-alter table hroest.srmTransitions add index(q3);
+alter table hroest.srmTransitions_test add index(parent_key);
+alter table hroest.srmTransitions_test add index(q3);
 
 #-- create a table that has bins in MS1 dimenstion of size 0.7 Da and in 
 #-- MS2 dimensions of size 1 Da and in SSRCalc dimension 4 units
-#drop table hroest.srmCollisions400710;
-#truncate table hroest.srmCollisions400710;
-create table hroest.srmCollisions400710(
+#drop table hroest.srmCollisions4010250_test;
+#truncate table hroest.srmCollisions4010250_test;
+create table hroest.srmCollisions4010250_test(
     coll_srm1 INT,
     coll_srm2 INT
 );
-alter table hroest.srmCollisions400710 add unique index(coll_srm1, coll_srm2);
+alter table hroest.srmCollisions4010250_test add unique index(coll_srm1, coll_srm2);
 
 drop table hroest.srm_counts;
 create temporary table hroest.srm_counts as
--- code/plot_q1q3space.R
+++ code/plot_q1q3space.R
@@ -2,29 +2,34 @@ library(RMySQL)
 dbh <- dbConnect( MySQL(),default.file='/etc/httpd/conf/.my.cnf')
 library(plotrix, lib.loc = '/projects/compep/R/')
 library(hexbin)
+result_dir <- "/home/hroest/srm_clashes/results/yeast/"
 
 df <- dbGetQuery(dbh, "
-select * from hroest.srmClashesHuman
-where experiment_key = 3061 
-and q1 between 400 and 1500 
+select q1, q3 
+from hroest.srmPeptides_new 
+inner join hroest.srmTransitions_new on parent_id = parent_key
+where 
+q1 between 400 and 1500 
 and q3 between 400 and 1500 
 ")
 
-#plot the q1-q3 space using hexbins
-pdf(file="./results/q1_q3_hexbins_120_human_small.pdf" )
-hxbinobj = hexbin(x=df[,7], y=df[,8], xbins=110 )
+#plot the q1-q3 space using hexbins -- first the small one
+pdf(file=paste(result_dir,"q1_q3_hexbins_120_small.pdf", sep="") )
+hxbinobj = hexbin(x=df[,1], y=df[,2], xbins=110 )
 plot( hxbinobj, xlab="", ylab="" )
 dev.off()
 
-pdf(file="./results/q1_q3_hexbins_1200_human_small.pdf" )
+##pdf(file=paste(result_dir,"q1_q3_hexbins_1200_small.pdf", sep="") )
 
-hxbinobj = hexbin(x=df[,7], y=df[,8], xbins=1100 )
+hxbinobj = hexbin(x=df[,1], y=df[,2], xbins=1100 )
 
-png(file="./results/q1_q3_hexbins_1200_human_small_bigfile.png", 
-    width = 5000, height = 5000, units = "px", res = 500 )
+#now the big one with 1200x1200 bins
+png(file=paste(result_dir,"q1_q3_hexbins_1200_small_bigfile.png", sep=""),
+    width = 3500, height = 3500, units = "px", res = 500 )
 plot( hxbinobj , xlab="", ylab="")
 dev.off()
-pdf(file="./results/q1_q3_hexbins_1200_human_small.pdf" )
+
+pdf(file=paste(result_dir,"q1_q3_hexbins_1200_small.pdf", sep="") )
 plot( hxbinobj , xlab="", ylab="")
 dev.off()
 
