dce7c5107bb2158957bee27794887714de226e71
Hannes Roest <hroest@rinaldo.(none)>
2010-08-25 17:44:31 +0200

    reduced size of main fxn for easier reuse

--- code/collider.py
+++ code/collider.py
@@ -1,27 +1,8 @@
 #!/usr/bin/python
-import os
-import MySQLdb
 import sys 
-sys.path.append( '/home/ghost/hroest/code/' )
-sys.path.append( '/home/ghost/software_libs/' )
-sys.path.append( '/home/hroest/lib/' )
 sys.path.append( '/home/hroest/msa/code/tppGhost' )
-sys.path.append( '/home/hroest/msa/code' )
-sys.path.append( '/home/hroest/srm_clashes/code' )
-#import mzXMLreader
-#import pepXMLReader
+sys.path.append( '/home/hroest/lib/' )
 import time
-from utils_h import utils
-#import pipeline 
-#db = MySQLdb.connect(read_default_file="~/hroest/.my.cnf")
-db = MySQLdb.connect(read_default_file="~/.my.cnf")
-c = db.cursor()
-c2 = db.cursor()
-cursor = db.cursor()
-import silver
-import DDB 
-import csv 
-R = silver.Residues.Residues('mono')
 import numpy
 import progress
 import gnuplot
@@ -104,10 +85,65 @@ def testcase():
     par.eval()
     return par
 
+def get_cum_dist(original):
+    cumulative = []
+    cum  = 0
+    for i, val in enumerate(original):
+        cum += val
+        cumulative.append(  cum )
+    return cumulative
+
 class SRMcollider(object):
-    def find_clashes(self, cursor, par):
+
+    def find_clashes(self, db, par):
         #make sure we only get unique peptides
         cursor = db.cursor()
+        self.pepids = self._get_unique_pepids(par, cursor)
+        self.allpeps = {}
+        self.allcollisions = []
+        self.q3min_distr = []
+        self.q1min_distr = []
+        self.q3min_distr_ppm = []
+        self.non_unique_count = 0
+        self.total_count = 0
+        start = time.time()
+        progressm = progress.ProgressMeter(total=len(self.pepids), unit='peptides')
+        for i, pep in enumerate(self.pepids):
+            p_id, q1, q1_charge, ssrcalc = pep
+            non_unique = {}
+            non_unique_q1 = {}
+            non_unique_ppm = {}
+            non_unique_clash = {}
+            all_clashes = {}
+            transitions = self._get_all_transitions(par, pep, cursor)
+            nr_transitions = len( transitions )
+            collisions = self._get_all_collisions(par, pep, cursor)
+            #here we loop through all possible combinations of transitions and
+            #potential collisions and check whether we really have a collision
+            q3_window_used = par.q3_window
+            for t in transitions:
+                if par.ppm: q3_window_used = par.q3_window * 10**(-6) * t[0]
+                this_min = q3_window_used
+                for c in collisions:
+                    if abs( t[0] - c[0] ) <= this_min:
+                        this_min = abs( t[0] - c[0] )
+                        non_unique[ t[1] ] = t[0] - c[0]
+                        non_unique_q1[ t[1] ] = q1 - c[1]
+                        non_unique_ppm[ t[1] ] = (t[0] - c[0] ) * 10**6 / t[0]
+                        all_clashes[ t[1] ] = c[2]
+            self.allpeps[ p_id ] = 1.0 - len( non_unique ) * 1.0  / nr_transitions
+            if len(non_unique) > 0:
+                self.allcollisions.extend( all_clashes.items() )
+                for v in non_unique.values(): self.q3min_distr.append( v )
+                for v in non_unique_q1.values(): self.q1min_distr.append( v )
+                for v in non_unique_ppm.values(): self.q3min_distr_ppm.append( v )
+            self.non_unique_count += len( non_unique )
+            self.total_count += len( transitions)
+            end = time.time()
+            progressm.update(1)
+        self.total_time = end - start
+
+    def _get_unique_pepids(self, par, cursor):
         query = """
         select parent_id, q1, q1_charge, ssrcalc
          from %s
@@ -118,24 +154,12 @@ class SRMcollider(object):
          %s
         """ % (par.peptide_table, par.peptide_table, par.query_add )
         cursor.execute( query )
-        self.pepids = cursor.fetchall()
-        f
-        self.allpeps = {}
-        self.q3min_distr = []
-        self.q3min_distr_ppm = []
-        self.non_unique_count = 0
-        self.total_count = 0
-        start = time.time()
-        progressm = progress.ProgressMeter(total=len(self.pepids), unit='peptides')
-        for i, pep in enumerate(self.pepids):
-            #if i % 100 ==0: print i
-            non_unique = {}
-            non_unique_clash = {}
-            non_unique_ppm = {}
+        return cursor.fetchall()
+
+    def _get_all_transitions(self, par, pep, cursor):
             p_id, q1, q1_charge, ssrcalc = pep
             q3_high = par.get_q3_high(q1, q1_charge)
             q3_low = par.get_q3_low()
-            #
             query1 = """
             select q3, srm_id
             from %(pep)s
@@ -146,11 +170,15 @@ class SRMcollider(object):
             """ % { 'parent_id' : p_id, 'q3_low' : q3_low,
                    'q3_high' : q3_high, 'query_add' : par.query1_add,
                    'pep' : par.peptide_table, 'trans' : par.transition_table }
-            nr_transitions = cursor.execute( query1 )
-            transitions = cursor.fetchall()
-            #
+            cursor.execute( query1 )
+            return cursor.fetchall()
+
+    def _get_all_collisions(self, par, pep, cursor):
+            p_id, q1, q1_charge, ssrcalc = pep
+            q3_high = par.get_q3_high(q1, q1_charge)
+            q3_low = par.get_q3_low()
             query2 = """
-            select q3
+            select q3, q1, srm_id
             from %(pep)s
             inner join %(trans)s
               on parent_id = parent_key
@@ -164,44 +192,33 @@ class SRMcollider(object):
                    'q3_low':q3_low,'q3_high':q3_high, 'q1_window' : par.q1_window,
                    'query_add' : par.query2_add, 'ssr_window' : par.ssrcalc_window,
                    'pep' : par.peptide_table, 'trans' : par.transition_table }
-            tmp = cursor.execute( query2 )
-            collisions = cursor.fetchall()
-            #here we loop through all possible combinations of transitions and
-            #potential collisions and check whether we really have a collision
-            #TODO it might even be faster to switch the loops
-            #TODO since the outer loop can be aborted as soon as a collision is found
-            q3_window_used = par.q3_window
-            for t in transitions:
-                if par.ppm: q3_window_used = par.q3_window * 10**(-6) * t[0]
-                this_min = q3_window_used
-                for c in collisions:
-                    if abs( t[0] - c[0] ) <= this_min:
-                        this_min = abs( t[0] - c[0] )
-                        non_unique[ t[1] ] = t[0] - c[0]
-                        non_unique_ppm[ t[1] ] = (t[0] - c[0] ) * 10**6 / t[0]
-            self.allpeps[ p_id ] = 1.0 - len( non_unique ) * 1.0  / nr_transitions
-            for v in non_unique.values(): self.q3min_distr.append( v )
-            for v in non_unique_ppm.values(): self.q3min_distr_ppm.append( v )
-            self.non_unique_count += len( non_unique )
-            self.total_count += len( transitions)
-            end = time.time()
-            progressm.update(1)
-        self.total_time = end - start
-    def store_in_from_file(self):
+            cursor.execute( query2 )
+            return cursor.fetchall()
+
+    def store_in_file(self):
         import pickle
-        pickle.dump( q3min_distr, open(common_filename + '_q3min_distr.pkl' , 'w'))
-        pickle.dump( q3min_distr_ppm, open(common_filename + '_q3min_distr_ppm.pkl', 'w'))
-        pickle.dump( allpeps, open(common_filename + '_allpeps.pkl', 'w'))
-        pickle.dump( [non_unique_count, total_count], open(common_filename + '_count.pkl', 'w'))
+        common_filename = par.get_common_filename()
+        try:
+            pickle.dump( self.q1min_distr, open(common_filename + '_q1min_distr.pkl' , 'w'))
+        except Exception: pass
+        pickle.dump( self.q3min_distr, open(common_filename + '_q3min_distr.pkl' , 'w'))
+        pickle.dump( self.q3min_distr_ppm, open(common_filename + '_q3min_distr_ppm.pkl', 'w'))
+        pickle.dump( self.allpeps, open(common_filename + '_allpeps.pkl', 'w'))
+        pickle.dump( [self.non_unique_count, self.total_count], open(common_filename + '_count.pkl', 'w'))
+
     def load_from_file(self, par, directory):
         import pickle
         #fname = directory + "yeast_True_False_20_250_100ppm_range300to2000"
         fname = directory + par.get_common_filename()
+        try:
+            self.q1min_distr = pickle.load( open(fname + "_q1min_distr.pkl"))
+        except Exception: pass
         self.q3min_distr = pickle.load( open(fname + "_q3min_distr.pkl"))
         self.q3min_distr_ppm = pickle.load( open(fname + "_q3min_distr_ppm.pkl"))
         self.allpeps = pickle.load( open(fname + "_allpeps.pkl"))
         self.non_unique_count, self.total_count = pickle.load( open(fname + "_count.pkl"))
         self.pepids = [ (k,) for k in self.allpeps.keys()]
+
     def print_unique_histogram(self, par):
         mydist = [  self.allpeps[ p[0] ] for p in self.pepids]
         h, n = numpy.histogram( mydist , 10)
@@ -213,6 +230,7 @@ class SRMcollider(object):
                                              tmp_csv = filename + '.csv' )
         #gnu.add_to_body( "set yrange[0:80]" )
         #gnu.draw_boxes(keep_data = True)
+
     def print_cumm_unique(self, par):
         mydist = [  self.allpeps[ p[0] ] for p in self.pepids]
         h, n = numpy.histogram( mydist , 100)
@@ -224,26 +242,40 @@ class SRMcollider(object):
         filename = par.get_common_filename() + "_cum"
         plt_cmd = 'with lines lt -1 lw 2'
         gnu = gnuplot.Gnuplot.draw_from_data( [cumh,n], plt_cmd, filename + '.eps',
-          'Unique transitions per peptide / %' , 'Cumulative Occurence / %', keep_data=True)
+          'Unique transitions per peptide / %' , 'Cumulative Occurence / %', 
+          keep_data=True, tmp_csv = filename + '.csv')
         gnu.add_to_body( "set yrange[0:100]" )
         gnu.draw(plt_cmd, keep_data=True)
         #title = '(2+,1+) transitions, Background of 4 combinations [Range 300 to 1500 Da]' )
+
     def print_q3min(self, par):
-        h, n = numpy.histogram( self.q3min_distr, 500)
-        reload( gnuplot )
-        n = [nn * 10**3 for nn in n]
+        h, n = numpy.histogram( self.q3min_distr, 100)
         filename = par.get_common_filename() + '_q3distr' 
+        n = [nn * 10**3 for nn in n]
         gnu  = gnuplot.Gnuplot.draw_boxes_from_data( [h,n], filename + '.eps',
           'Q3 difference / 10^{-3} Th', 'Number of transitions', keep_data = True )
         #gnu.add_to_body( "set xrange[%s:%s]"  % (-1, 1) )
         #gnu.add_to_body( "set yrange[0:100]" )
         #gnu.draw_boxes()
+
     def print_q3min_ppm(self, par):
-        h, n = numpy.histogram( self.q3min_distr_ppm, 100, (-5, 5) )
+        h, n = numpy.histogram( self.q3min_distr_ppm, 100, (-par.q3_window, par.q3_window) )
         filename = par.get_common_filename() + '_q3distr_ppm' 
         gnu  = gnuplot.Gnuplot.draw_boxes_from_data( [h,n], filename + '.eps',
           'Q3 difference / ppm', 'Number of transitions', keep_data = True )
         #gnu.add_to_body( "set xrange[%s:%s]"  % (-0.1, 0.1) )
         #gnu.add_to_body( "set xrange[%s:%s]"  % (-5, 5) )
         #gnu.draw_boxes()
+
+    def print_stats(self):
+        print "Nonunique / Total transitions : %s / %s = %s" % (
+            self.non_unique_count, self.total_count, 
+            self.non_unique_count * 1.0 /self.total_count)
+        mydist = [  self.allpeps[ p[0] ] for p in self.pepids]
+        h, n = numpy.histogram( mydist , 100)
+        print('Percentage of collisions below 1 ppm: %02.2f~\%%' % (sum( h[40:60] )* 100.0 / sum( h )))
+
+
+
+
 
