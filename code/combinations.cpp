8cc1ab3ba9c247ac22581cbed7bb3c65c585d81a
hr <hroest@ethz.ch>
2010-10-15 17:35:35 +0200

    polished code, commented, made string mapping possible;

--- code/combinations.cpp
+++ code/combinations.cpp
@@ -1,27 +1,55 @@
-#include <Python.h>
+/*
+ * The functions in this file calculate all combinations of M elements drawn
+ * without replacement from a set of N elements.  Order of elements does NOT
+ * matter.
+ *
+ * The function Display is used to write to a ofstream which was defined
+ * before. Instead of writing the indices, a string mapping can be provided.
+ *
+ * The function _combinations is used to calculate the indices and call Display
+ *
+ * The function _combinations_wrapper can be called from Python. It opens the
+ * file and converts the python string-list mapping into a C++ vector<string>
+ *
+ * Example: M = 2, N = 4, mapping = ['Spam', 'Eggs', 'Bacon', 'Sausage' ]
+ * Result:
+ * Spam, Eggs
+ * Spam, Bacon
+ * Spam, Sausage
+ * Eggs, Bacon
+ * Eggs, Sausage
+ * Bacon, Sausage
+*/
+
 #include <ctime>
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
-void Display(int vi[], int size, ofstream &myfile)
-{
-    for(size_t i=0; i<size; ++i)
-        myfile<<vi[i]<<",";
-    myfile<<endl;
+void Display(int vi[], int size, ofstream &myfile, const vector<string>&
+        string_mapping) {
+    for(int i=0; i<size-1; ++i)
+        myfile << string_mapping[vi[i]] << ",";
+    myfile << string_mapping[vi[size-1]];
+    myfile << endl;
 }
 
-
-
-void _combinations(int M, int N, ofstream &myfile) {
+void _combinations(int M, int N, ofstream &myfile, const vector<string>&
+        string_mapping) {
+    // The basic idea is to create an index array of length M that contains
+    // numbers between 0 and N. The indices denote the combination produced and
+    // we always increment the rightmost index. If it goes above N, we try to
+    // increase the one left to it until we find one that still can be
+    // increased. We stop when the rightmost index hits N-M, we thus go from
+    // (0,1,2,3...M-1) to (N-M,N-M+1,N-M+2...N-1)
     int j, k;
     int* index = new int[M];
-    //initialize with number from 0 to M = range( M )
-    for(int k=0;k<M;k++) index[k] =  k ;
+    //initialize with numbers from 0 to M = range( M )
+    for(int k=0;k<M;k++) index[k] = k;
     while (index[0] < N-M) {
-        Display( index , M, myfile);
-        //cout << index[0] << N-M << endl;
+        Display( index, M, myfile, string_mapping);
         index[ M-1 ] += 1;
         if (index[ M-1 ] >= N) {
             //#now we hit the end, need to increment other positions than last
@@ -34,27 +62,29 @@ void _combinations(int M, int N, ofstream &myfile) {
             while (k < M) { index[k] = index[k-1] + 1; k += 1; } 
         }
     }
-    Display( index , M, myfile);
+    Display( index, M, myfile, string_mapping);
     delete [] index;
 }
 
-void _combinations_wrapper(int M, int N, const char* filename) {
-    ofstream myfile;
-    myfile.open (filename);
-    _combinations( M, N, myfile);
-    myfile.close();
-}
 
 
-/*
-int main()
+/* int main()
 {
     cout << "starting \n";
 
     clock_t start, finish;
     start = clock();
 
-    _combinations_wrapper(3,6, "test.out");
+    vector<string> mystrings(4);
+    mystrings[0] =  "Spam";
+    mystrings[1] = "Eggs";
+    mystrings[2] =  "Bacon";
+    mystrings[3] =  "Sausage";
+
+    ofstream myfile;
+    myfile.open ("test.out");
+    _combinations( 2, 4, myfile, mystrings);
+    myfile.close();
 
     cout << "done \n";
     finish = clock();
@@ -63,19 +93,63 @@ int main()
     cout << ( (finish - start) * 1.0 /CLOCKS_PER_SEC ) << endl;
 
 return 0;
-}
+} */
 
+
+
+
+
+
+/*
+ *
+ *
+ * PYTHON interface
 */
+
+#include <boost/python.hpp>
+#include <boost/python/module.hpp>
+#include <boost/python/def.hpp>
+
+void _combinations_wrapper(int M, int N, const char* filename,
+        boost::python::list mapping) {
+    ofstream myfile;
+    myfile.open (filename);
+
+    int len = boost::python::extract<int>(mapping.attr("__len__")());
+    //Do Error checking, the mapping needs to be at least as long as N 
+    if (len < N) {
+        PyErr_SetString(PyExc_ValueError, 
+            "The string mapping must be at least of length N");
+        boost::python::throw_error_already_set();
+        return;
+    }
+    //Also M should not be bigger than N (not much point) and not smaller than 0
+    if(M>N) {
+        PyErr_SetString(PyExc_ValueError, 
+            "M cannot be larger than N");
+        boost::python::throw_error_already_set();
+        return;
+    }
+    if(N < 0 || M < 0) {
+        PyErr_SetString(PyExc_ValueError, 
+            "M and N need to be larger than 0");
+        boost::python::throw_error_already_set();
+        return;
+    }
+
+    vector<string> mystrings(len);
+    for (int i=0; i<len; i++) {
+        mystrings[i] = boost::python::extract<char const *>(mapping[i]);
+    }
+
+    _combinations( M, N, myfile, mystrings);
+    myfile.close();
+}
 
 void initcombinations() {;}
 
-
-
-#include <boost/python/module.hpp>
-#include <boost/python/def.hpp>
 using namespace boost::python;
-
-BOOST_PYTHON_MODULE(h_combinations)
+BOOST_PYTHON_MODULE(c_combinations)
 {
     def("combinations", _combinations_wrapper);
 }
