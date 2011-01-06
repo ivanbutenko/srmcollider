58f5572ece9c4b711500876720fb73ce2a54ab1c
Hannes Roest <hroest@rinaldo.(none)>
2011-01-06 11:11:16 +0100

    added another core function: populate non_unique dict

--- code/getNonUis.cpp
+++ code/getNonUis.cpp
@@ -101,15 +101,77 @@ python::dict _getnonuis_wrapper(python::tuple transitions,
     return collisions_per_peptide;
 }
 
+
+python::dict _find_clashes_core_non_unique(python::tuple transitions,
+        python::tuple collisions, double q3window, bool ppm) {
+
+    python::dict non_unique;
+    python::tuple clist;
+    python::tuple tlist;
+
+    int collision_length = python::extract<int>(collisions.attr("__len__")());
+    int transitions_length = python::extract<int>(transitions.attr("__len__")());
+    long t1 ;
+    double t0, c0, q3used = q3window, this_min;
+
+
+    for (int i=0; i<transitions_length; i++) {
+        tlist = python::extract< python::tuple >(transitions[i]);
+
+        //ppm is 10^-6
+        t0 = python::extract< double >(tlist[0]);
+        if(ppm) {q3used = q3window / 1000000.0 * t0; } 
+        this_min = q3used;
+
+        // go through all (potential) collisions
+        // and store closest collision in Q3 as a dictionary entry
+        for (int j=0; j<collision_length; j++) {
+            clist = python::extract< python::tuple >(collisions[j]);
+            c0 = python::extract< double >(clist[0]);
+
+            if(fabs(t0-c0) <  this_min) {
+
+                t1 = python::extract<long>(tlist[1]);
+                this_min = fabs(t0-c0);
+                non_unique[t1] = this_min;
+            }
+        }
+    }
+
+    /*
+     * In Python, this corresponds to the following function
+     *
+            for t in transitions:
+                if par.ppm: q3_window_used = par.q3_window * 10**(-6) * t[0]
+                this_min = q3_window_used
+                for c in collisions:
+                    if abs( t[0] - c[0] ) <= this_min:
+                        non_unique[ t[1] ] = t[0] - c[0]
+            */
+
+    return non_unique;
+}
+
+
+
+
 void initgetnonuis() {;}
+void initcore_non_unique() {;}
+void initget_non_uis() {;}
 
 using namespace python;
 BOOST_PYTHON_MODULE(c_getnonuis)
 {
+
     def("getnonuis", _getnonuis_wrapper, 
+ "void tesat()\n"
+           );
 
  "void tesat()\n"
 
+    def("core_non_unique", _find_clashes_core_non_unique, 
+ "void tesat()\n"
            );
+
 }
 
