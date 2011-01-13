e7e86c3803b00b9c2c6e6dbda3a348194ac4d5c7
Hannes Roest <hroest@rinaldo.(none)>
2011-01-13 16:06:07 +0100

    setup script

--- code/csetup.py
+++ code/csetup.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 
-#instructions: python csetup.py build
-#to see whether it works:
-
 """
-import string
-import c_combinations
-c_combinations.combinations(5, len(string.letters), 
-                            'test.out', [l for l in string.letters] )
+instructions: python csetup.py build
+sudo apt-get install python-dev
+sudo apt-get install libcgal-dev
+sudo apt-get install libboost-python-dev 
+sudo apt-get install 
 """
 
 #these 2.5M combinations should be written to "test.out" in a couple of seconds
@@ -17,14 +15,17 @@ c_combinations.combinations(5, len(string.letters),
 from distutils.core import setup
 from distutils.extension import Extension
  
-setup(name="c_combinations",
+setup(name="srmcollider",
+    url = "http://www.srmcollider.org", 
+    version = "0.7",
+    author = "Hannes Roest",
     ext_modules=[
         Extension("c_combinations", ["combinations.cpp"],
-        libraries = ["boost_python"])
-    ])
-
-setup(name="c_getnonuis",
-    ext_modules=[
+            libraries = ["boost_python"]),
         Extension("c_getnonuis", ["getNonUis.cpp"],
-        libraries = ["boost_python"])
+            libraries = ["boost_python"]),
+        Extension("r_rangetree", ["rangetree.cpp"],
+            libraries = ["boost_python", "CGAL"]),
     ]) 
+
+
