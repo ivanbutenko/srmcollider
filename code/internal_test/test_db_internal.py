f37edc16ceb1326f07b92f54f2d824cc3427279e
HR <you@example.com>
2012-09-25 11:22:28 +0200

    move internal tests into internal section: 66 + 17 internal + 7 CTest

--- code/internal_test/test_db_internal.py
+++ code/internal_test/test_db_internal.py
@@ -9,6 +9,8 @@ import unittest
 import sys
 sys.path.append( '..')
 sys.path.append( '../external')
+sys.path.append( '../test')
+sys.path.append( 'test')
 import collider
 from Residues import Residues
 
@@ -214,130 +216,182 @@ class Test_collider_mysql(unittest.TestCase):
                         collisions_per_peptide[c[3]] = [ t[1] ] ; 
         return collisions_per_peptide
 
-    @check_cgetnonuis_availability
     @attr('slow') 
-    def test_complete_without_isotopes(self):
+    def test_get_all_precursors1(self): 
 
         if not self.database_available: return
 
-        par = collider.testcase(testdatabase=test_database)
-        par.query2_add = ' and isotope_nr = 0 ' # still necessary, old style tables
-        par.quiet = True
-        mycollider = collider.SRMcollider()
+        pep = test_shared.runpep1
+        pep = test_shared.runpep_obj1
+        transitions = test_shared.runtransitions1
+        precursors = test_shared.runprecursors1
+
+        # filter out the isotopes
+        pprec = []
+        for p in precursors:
+          if(p[1] not in [pp[1] for pp in pprec]): pprec.append(p)
+        precursors = pprec
+        runprecursors_obj2 = []
+        for p in precursors:
+            runprecursors_obj2.append(Precursor( q1 = p[0], q1_charge = p[3],
+            modified_sequence = p[1], transition_group = p[2], isotopically_modified = p[4]))
+        precursors = runprecursors_obj2
+
+        transitions = tuple([ (t[0], i) for i,t in enumerate(transitions)])
+        par = self.par
+
         cursor = self.db.cursor()
+        myprec = collider.SRMcollider()._get_all_precursors(par, pep, cursor)
+        self.assertEqual( len(myprec), len(precursors))
 
-        #for historic reasons, we only select a subset of peptides
-        exclude_pepids = [183, 203, 319, 345, 355, 365, 367, 385, 393, 425, 1227, 1233, 1297, 1299, 1303, 1305, 1307, 1309, 1311, 1509, 1681, 1683]
-        mycollider = collider.SRMcollider()
-        pepids = _get_unique_pepids(par, cursor, ignore_genomeoccurence=True)
-        pepids = [p for p in pepids if p['parent_id'] not in exclude_pepids]
+        precursors.sort(psort)
+        myprec.sort(psort)
 
-        find_clashes_small(self, mycollider, cursor, par, pepids)
-        do_check_complete(self, mycollider)
+        for i in range(len(myprec)):
+            self.assertEqual( myprec[i].modified_sequence, precursors[i].modified_sequence)
+            self.assertEqual( myprec[i].q1_charge, precursors[i].q1_charge)
 
-    @check_cgetnonuis_availability
     @attr('slow') 
-    def test_complete_with_isotopes(self):
+    def test_get_all_precursors2(self): 
 
         if not self.database_available: return
 
-        #now test with isotopes enabled
-        par  = collider.testcase(testdatabase=test_database)
-        par.quiet = True
-        par.isotopes_up_to = 3
-        par.eval()
-        par.query2_add = ' and isotope_nr = 0 ' # need to do this because we also have other isotopes in there!
-        mycollider = collider.SRMcollider()
+        pep = test_shared.runpep_obj2
+        transitions = test_shared.runtransitions2
+        precursors = test_shared.runprecursors2
+
+        # filter out the isotopes
+        pprec = []
+        for p in precursors:
+          if(p[1] not in [pp[1] for pp in pprec]): pprec.append(p)
+        precursors = pprec
+        runprecursors_obj2 = []
+        for p in precursors:
+            runprecursors_obj2.append(Precursor( q1 = p[0], q1_charge = p[3],
+            modified_sequence = p[1], transition_group = p[2], isotopically_modified = p[4]))
+        precursors = runprecursors_obj2
+
+        transitions = tuple([ (t[0], i) for i,t in enumerate(transitions)])
+        par = self.par
+
         cursor = self.db.cursor()
+        myprec = collider.SRMcollider()._get_all_precursors(par, pep, cursor)
+        self.assertEqual( len(myprec), len(precursors))
 
-        #for historic reasons, we only select a subset of peptides
-        exclude_pepids = [183, 203, 319, 345, 355, 365, 367, 385, 393, 425, 1227, 1233, 1297, 1299, 1303, 1305, 1307, 1309, 1311, 1509, 1681, 1683]
-        mycollider = collider.SRMcollider()
-        pepids = _get_unique_pepids(par, cursor, ignore_genomeoccurence=True)
-        pepids = [p for p in pepids if p['parent_id'] not in exclude_pepids]
+        precursors.sort(psort)
+        myprec.sort(psort)
 
-        find_clashes_small(self, mycollider, cursor, par, pepids) 
+        for i in range(len(myprec)):
+            self.assertEqual( myprec[i].modified_sequence, precursors[i].modified_sequence)
+            self.assertEqual( myprec[i].q1_charge, precursors[i].q1_charge)
 
-        assert abs(mycollider.allpeps[111] - 0.888888888889) < 1e-5
-        assert abs(mycollider.allpeps[191] - 0.7) < 1e-5
-        assert abs(mycollider.allpeps[375] - 0.941176470588) < 1e-5
-        assert abs(mycollider.allpeps[585] - 0.875) < 1e-5
-        assert abs(mycollider.allpeps[609] - 0.6666666) < 1e-5
-        assert abs(mycollider.allpeps[929] - 0.857142857143) < 1e-5
-        assert abs(mycollider.allpeps[1089] - 0.7) < 1e-5
-        assert abs(mycollider.allpeps[1101] - 0.916666666667) < 1e-5
-        assert abs(mycollider.allpeps[1177] - 0.928571428571) < 1e-5
-        assert abs(mycollider.allpeps[1493] - 0.928571428571) < 1e-5
-        assert abs(mycollider.allpeps[1537] - 0.857142857143) < 1e-5
-        assert abs(mycollider.allpeps[1585] - 0.933333333333) < 1e-5
-        assert abs(mycollider.allpeps[1663] - 0.928571428571) < 1e-5
-        assert abs(mycollider.allpeps[1763] - 0.8125) < 1e-5
-        assert abs(mycollider.allpeps[1925] - 0.928571428571) < 1e-5
-        assert abs(mycollider.allpeps[1957] - 0.833333333333) < 1e-5
-        assert abs(mycollider.allpeps[1967] - 0.857142857143) < 1e-5
-        assert abs(mycollider.allpeps[1991] - 0.857142857143) < 1e-5
-
-        self.assertTrue( abs(sum( mycollider.allpeps.values() ) - 971.215436) < 10**(-3) )
-        self.assertEqual( len([v for v in mycollider.allpeps.values() if v == 1.0 ] ), 922 )
-
-        self.assertEqual( mycollider.non_unique_count,  71)
-        self.assertEqual( mycollider.total_count, 12502 )
-
-class Test_collider_sqlite(unittest.TestCase):
-
-    def setUp(self):
-
-        try:
-            import sqlite
-            from sqlite import DatabaseError
-            self.database_available = True
-        except ImportError:
-            print """Module sqlite not available.
-            
-            Please install it if you want to use it.
-            Use the following command (on Ubuntu systems):
-                sudo apt-get install python-sqlite
-            """
-            self.database_available = False
-
-        try:
-            #the database file must exist and the databases must be created
-            self.db = sqlite.connect(test_shared.SQLITE_DATABASE_LOCATION)
-            cursor = self.db.cursor()
-            cursor.execute("select count(*) from srmPeptides_test")
-            #print cursor.fetchall()
-        except DatabaseError:
-            print 
-            print "=" * 75
-            print """The sqlite database is not available.
-            
-            Please run the sqlite_setupdb.py script first."""
-            print "=" * 75
-            self.database_available = False
-
-    def tearDown(self):
-      self.db.close()
-
-    @check_cgetnonuis_availability
-    def test_1(self):
+    @attr('slow') 
+    def test_get_coll_per_peptide(self): 
 
         if not self.database_available: return
-        verbose = False
-        par = collider.testcase(testdatabase=test_database)
-        #par.query2_add = ' and isotope_nr = 0 ' # still necessary, old style tables
-        par.quiet = True
-        par.transition_table = 'srmTransitions_test'
-        par.peptide_tables = ['srmPeptides_test']
+
+        pep = test_shared.runpep_obj1
+        transitions = test_shared.runtransitions1
+        precursors = test_shared.runprecursors1
+        transitions = tuple([ (t[0], i) for i,t in enumerate(transitions)])
+        par = self.par
+        q3_high = self.q3_high
+        q3_low = self.q3_low
+        R = self.R
         cursor = self.db.cursor()
 
-        #for historic reasons, we only select a subset of peptides
-        exclude_pepids = [183, 203, 319, 345, 355, 365, 367, 385, 393, 425, 1227, 1233, 1297, 1299, 1303, 1305, 1307, 1309, 1311, 1509, 1681, 1683]
-        mycollider = collider.SRMcollider()
-        pepids = _get_unique_pepids(par, cursor, ignore_genomeoccurence=True)
-        pepids = [p for p in pepids if p['parent_id'] not in exclude_pepids]
+        collisions_per_peptide = collider.get_coll_per_peptide(self.acollider, 
+           transitions, par, pep, cursor)
+        self.assertEqual(len(collisions_per_peptide), len(test_shared.collpepresult1))
+        for k in collisions_per_peptide.keys():
+          self.assertEqual(collisions_per_peptide[k], test_shared.collpepresult1[k])
+        self.assertEqual(collisions_per_peptide, test_shared.collpepresult1)
 
-        find_clashes_small(self, mycollider, cursor, par, pepids)
-        do_check_complete(self, mycollider)
+        collisions_per_peptide = collider.get_coll_per_peptide(self.acollider, 
+           transitions, par, pep, cursor, forceNonCpp=True)
+        self.assertEqual(collisions_per_peptide, test_shared.collpepresult1)
+
+    @attr('slow') 
+    def test_get_coll_per_peptide2(self): 
+
+        if not self.database_available: return
+
+        pep = test_shared.runpep_obj2
+        transitions = test_shared.runtransitions2
+        precursors = test_shared.runprecursors2
+        transitions = tuple([ (t[0], i) for i,t in enumerate(transitions)])
+
+        par = self.par
+        q3_high = self.q3_high
+        q3_low = self.q3_low
+        R = self.R
+        cursor = self.db.cursor()
+
+        collisions_per_peptide = collider.get_coll_per_peptide(self.acollider, 
+           transitions, par, pep, cursor)
+        self.assertEqual(len(collisions_per_peptide), len(test_shared.collpepresult2))
+        for k in collisions_per_peptide.keys():
+          self.assertEqual(collisions_per_peptide[k], test_shared.collpepresult2[k])
+        self.assertEqual(collisions_per_peptide, test_shared.collpepresult2)
+
+        collisions_per_peptide = collider.get_coll_per_peptide(self.acollider, 
+           transitions, par, pep, cursor, forceNonCpp=True)
+        self.assertEqual(len(collisions_per_peptide), len(test_shared.collpepresult2))
+        self.assertEqual(collisions_per_peptide, test_shared.collpepresult2)
+
+    def test_get_coll_per_peptide_debug(self): 
+        """This is a small example with very few interferences, 
+        good to debug"""
+
+        if not self.database_available: return
+
+        pep = test_shared.runpep_obj2
+        transitions = test_shared.runtransitions2
+        precursors = test_shared.runprecursors2
+        transitions = tuple([ (t[0], i) for i,t in enumerate(transitions)])
+
+        self.par.q3_window = 0.1 / 2.0
+        self.par.q1_window = 0.1 / 2.0
+        self.par.ssrcalc_window = 2.0 / 2.0 
+        self.par.query2_add = ''
+        self.par.peptide_tables = [test_database + '.srmPeptides_human']
+        self.par.query2_add = ' and isotope_nr = 0'  # necessary because of old style tables
+        self.par.print_query = False
+
+        # Our q1 from peptide 'ELNQLEDK' is 494.751462374, the q1 of the
+        # interfering NISVAEVEK is 494.769652374
+        #
+        # Our 5 transitions from peptide 'ELNQLEDK' are
+        # [3, 5, 6, 9, 14]
+        # [(504.26693971600002, 3), (357.17740503200002, 5), (485.23598503200003,
+        #   6), (842.38957503200004, 9), (421.69870003200003, 14)]
+        # The 5 transitions of the interfering peptide are
+        #
+        # 21406312 |   359414 |  1 | 504.266939716 | y4 | 
+        # 21406339 |   359414 |  2 | 357.195595032 | b7 | 
+        # 21406321 |   359414 |  1 | 485.272365032 | b5 | 
+        # 21406324 |   359414 |  1 | 842.425955032 | b8 | 
+        # 21406340 |   359414 |  2 | 421.716890032 | b8 | 
+        #
+
+        par = self.par
+        q3_high = self.q3_high
+        q3_low = self.q3_low
+        R = self.R
+        cursor = self.db.cursor()
+        collisions_per_peptide = collider.get_coll_per_peptide(self.acollider, 
+           transitions, par, pep, cursor)
+
+        self.assertEqual(len(collisions_per_peptide[359414]), 5)
+        self.assertEqual(collisions_per_peptide[359414], [3, 5, 6, 9, 14])
+        
+        self.assertEqual(59, len(collisions_per_peptide))
+        self.assertEqual(19, len([k for k,v in collisions_per_peptide.iteritems() if len(v) == 1]))
+        self.assertEqual(25, len([k for k,v in collisions_per_peptide.iteritems() if len(v) == 2]))
+        self.assertEqual(6, len([k for k,v in collisions_per_peptide.iteritems() if len(v) == 3]))
+        self.assertEqual(8, len([k for k,v in collisions_per_peptide.iteritems() if len(v) == 4]))
+        self.assertEqual(1, len([k for k,v in collisions_per_peptide.iteritems() if len(v) == 5]))
+        self.assertEqual(0, len([k for k,v in collisions_per_peptide.iteritems() if len(v) == 6]))
 
 if __name__ == '__main__':
     unittest.main()
---
+++ code/test/sqltest_tables_setup.py
@@ -0,0 +1,68 @@
+import MySQLdb
+import sqlite
+import test_shared
+import test_db, sys
+
+if sys.argv[1] == "mysql":
+    try: 
+      conn = MySQLdb.connect(read_default_file=test_db.mysql_conf_file)
+      c = conn.cursor()
+      c.connection.autocommit(True)
+    except MySQLdb.OperationalError as e:
+        print "Could not connect to database: Please check the configuration in test/test_db.py!\n", e
+        sys.exit()
+elif sys.argv[1] == "sqlite":
+    conn = sqlite.connect(test_shared.SQLITE_DATABASE_LOCATION)
+    c = conn.cursor()
+else:
+    print "First argument must be either 'mysql' or 'sqlite'"
+    sys.exit()
+
+table = 'srmPeptides_test'
+
+try:
+    c.execute("DROP TABLE %s" % table)
+except MySQLdb.OperationalError as e:
+    # If the table doesnt exist, mysql throws an error. We cannot use IF EXISTS
+    # in the SQL query because Sqlite doesnt understand that!
+    pass
+
+c.execute(
+"""
+create table %(table)s(
+    parent_id INT PRIMARY KEY ,
+    peptide_key INT,
+    modified_sequence VARCHAR(255),
+    q1_charge TINYINT,
+    q1 DOUBLE,
+    ssrcalc DOUBLE,
+    isotope_nr TINYINT,
+    transition_group INT,
+
+    modifications TINYINT UNSIGNED,
+    missed_cleavages TINYINT UNSIGNED,
+    isotopically_modified TINYINT UNSIGNED
+); """ % {'table' : table})
+c.execute("create index testpepkey   on %(table)s (peptide_key);" % {'table': table})
+c.execute("create index testq1       on %(table)s (q1)" % {'table': table})
+c.execute("create index testssrcalc  on %(table)s (ssrcalc)" % {'table': table})
+c.execute("create index testtrgroup  on %(table)s (transition_group)" % {'table': table})
+
+import csv
+creader = csv.reader( open('sqltestp.out'), delimiter='\t')
+#creader.next()
+for rr in creader:
+    # skip all that have isotope_nr other than zero (we used to store higher
+    # isotopes explicitely but dont do this any more)
+    if int(rr[6]) != 0: continue
+    query = 'insert into %s' % table + """
+( parent_id , peptide_key, modified_sequence, q1_charge, q1, ssrcalc, isotope_nr, transition_group, isotopically_modified, modifications, missed_cleavages)
+values (%s, %s, '%s', %s, %s, %s, %s, %s, 0, 0, 0)
+        """ % (rr[0], rr[1], rr[2], rr[3], rr[4], rr[5], rr[6] , rr[7] )
+    c.execute(query)
+conn.commit()
+
+c.close()
+exit()
+
+
