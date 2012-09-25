c8040001e219b043d0c949bb46173bc20d3e5505
HR <you@example.com>
2012-09-25 17:45:35 +0200

    remove troubling C code from release

--- code/Release/do_release.sh
+++ code/Release/do_release.sh
@@ -4,4 +4,5 @@ cp ../runcollider.py
 python setup.py sdist --manifest-only
 python setup.py sdist --formats=gztar,zip
 patch -p1 < Release/Release_patch1.patch
+patch -p1 < Release/Release_patch2.patch
 
